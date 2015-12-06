e953224bdd52f552cfbcaa02490b9a4a9b94e9f0
tangkun <tangkun.tk@alibaba-inc.com>
2015-12-06 11:17:39 +0800

    完成死锁部分的，协作间的死锁 和 饥饿死锁的例子

--- src/main/java/com/java/base/deadlock/HelpDeadLock.java
+++ src/main/java/com/java/base/deadlock/HelpDeadLock.java
@@ -2,8 +2,77 @@ package com.java.base.deadlock;
 
 /**
  * Created by tangkun.tk on 2015/12/5.
- * 协助对象间的死锁
+ * 协助对象间的死锁(相互调用)
  */
 public class HelpDeadLock {
 
+    private final Dispatch dispatch;
+    private String name = "a";
+    public HelpDeadLock(Dispatch dispatch){
+        this.dispatch = dispatch;
+    }
+
+    public synchronized void syncSetName(String name){
+        this.name = name;
+    }
+
+    public synchronized void callDispatch(){
+        try {
+            System.out.println("callDispatch waiting.....");
+            Thread.sleep(2*1000L);
+            dispatch.notifyHelpDeadLock(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        final Dispatch dispatch = new Dispatch();
+        final HelpDeadLock deadLock = new HelpDeadLock(dispatch);
+        dispatch.setDeadLock(deadLock);
+
+        new Thread(new Runnable() {
+            public void run() {
+                dispatch.changeName();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                deadLock.callDispatch();
+            }
+        }).start();
+
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+class Dispatch{
+
+    private HelpDeadLock deadLock;
+
+    public synchronized void notifyHelpDeadLock(HelpDeadLock helpDeadLock){
+        helpDeadLock.setName("b");
+    }
+
+    public synchronized void changeName(){
+        System.out.println("changeName waiting.....");
+        try {
+            Thread.sleep(2 * 1000L);    // sleep 2 second,waiting sync
+            deadLock.syncSetName("c");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDeadLock(HelpDeadLock deadLock) {
+        this.deadLock = deadLock;
+    }
+}
+
+
--- src/main/java/com/java/base/deadlock/HungerDeadLock.java
+++ src/main/java/com/java/base/deadlock/HungerDeadLock.java
@@ -1,9 +1,43 @@
 package com.java.base.deadlock;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * Created by tangkun.tk on 2015/12/5.
  * 线程饥饿死锁
+ * 线程池里只有1个线程,但提交的第一个任务依赖于第二个和第三个任务执行完成。
+ * 线程池中线程永远执行不到第二个第三个任务,因而造成死锁.
  */
 public class HungerDeadLock {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+    public static void main(String[] args) {
+        HungerDeadLock deadLock = new HungerDeadLock();
+        RenderPageTask renderPageTask = deadLock.new RenderPageTask();
+        deadLock.exec.submit(renderPageTask);
+    }
+
+    public  class RenderPageTask implements Callable<String>{
+        public String call() throws Exception {
+            System.out.println("RenderPageTask.call()");
+            Future header = exec.submit(new LoadTask("header"));
+            Future footer = exec.submit(new LoadTask("header"));
+            return (String)header.get()+footer.get();
+        }
+    }
+
+    static class LoadTask implements Runnable{
+        private String s;
+        public LoadTask(String s){
+            this.s = s;
+        }
+
+        public void run() {
+            System.out.println(s + " ok ");
+        }
+    }
+
 
 }
